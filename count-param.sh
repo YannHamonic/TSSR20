ccef390b7981773e5369e46930097a4a9cea84fd
YannHamonic <128361827+YannHamonic@users.noreply.github.com>
2023-03-20 12:12:24 +0100

    Upload file count-param.sh
    Initial script

---
+++ count-param.sh
@@ -0,0 +1,11 @@
+##!/bin/bash
+
+# Script de dénombrage des paramètres
+# Auteur : Y. Hamonic
+# Date de création : 20/03/2023
+
+# Entrée : Paramètres au nombre indéfinie
+# Sortie : Nombre de paramètres entrées
+
+count="$#"
+echo "Nombre d'arguments d'appel = $count"
